c8441cc6aecc396d424a31844a66c3f3ebd3db47
chrisrawlings <c.d.rawlings@gmail.com>
2019-11-30 11:37:54 -0500

    email added. Error pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash
+from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -8,13 +9,15 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-
+mail = Mail(app)
 
 
 @app.route('/')
 def index():
+    msg = Message("Hello", sender='dev@rawlings.site', recipients=['c.d.rawlings@gmail.com'])
+    msg.body = 'this is the test message'
+    mail.send(msg)
     return render_template('index.html')
-
 
 @app.route('/dashboard')
 def dashboard():
@@ -36,6 +39,16 @@ def newuser():
         flash('Login requested for user {}'.format(form.name.data, ))
         return redirect(url_for('dashboard'))
     return render_template('new_user.html', title='create new user', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -4,6 +4,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://rawlings:5eDnD@localhost:8889/char"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.rawlings.site'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True,
+    MAIL_USERNAME = 'dev@rawlings.site'
+    MAIL_PASSWORD = 'Test1Test'
+    MAIL_DEBUG = False
+    MAIL_SUPPRESS_SEND = False
+    TESTING = False
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
